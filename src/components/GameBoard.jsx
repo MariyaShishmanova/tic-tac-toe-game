9eeeb38088aca65f79c8ad89b202c7d4604da161
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-06-16 15:25:42 +0300

    fix gameBoard and Players components

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
+const initialGameBoard = [[null, null, null], [null, null, null], [null, null, null]];
 
 export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard(prevGameBoard => {
-      const updatedBoard = prevGameBoard.map((row, rIdx) =>
-        row.map((cell, cIdx) => {
-          if (rIdx === rowIndex && cIdx === colIndex && cell === null) {
-            return activePlayerSymbol;
-          }
-          return cell;
-        })
-      );
+      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
 
@@ -27,19 +17,19 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) =>
         <li key={rowIndex}>
           <ol>
-            {row.map((playerSymbol, colIndex) => (
+            {row.map((playerSymbol, colIndex) =>
               <li key={colIndex}>
                 <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
-            ))}
+            )}
           </ol>
         </li>
-      ))}
+      )}
     </ol>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,37 +1,42 @@
 import { useState } from 'react';
 
 export default function Player({ initialName, symbol, isActive }) {
-  const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing(editing => !editing);
   }
 
-  function handleChange(event) {
+  function handleChange(event){
     setPlayerName(event.target.value);
   }
 
-  let editablePlayerName = (
+  let editableName = (
     <span className="player-name">
       {playerName}
     </span>
   );
 
+  let btnCaption = 'Edit';
+
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    editableName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    btnCaption = 'Save';
   }
 
   return (
     <li className={isActive ? "active" : undefined}>
       <span className="player">
-        {editablePlayerName}
+        {editableName}
+
         <span className="player-symbol">
           {symbol}
         </span>
       </span>
-      <button onClick={handleEditClick} className="edit-button">
-        {isEditing ? 'Save' : 'Edit'}
+
+      <button onClick={handleEditClick}>
+        {btnCaption}
       </button>
     </li>
   );
